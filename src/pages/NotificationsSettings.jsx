9069640ab8dd68e6429d09c82ac0f1e2ddcb8290
crea8ivedev <crea8ivedev@gmail.com>
2025-06-12 19:05:54 +0530

    feat: user notification setting

--- src/pages/NotificationsSettings.jsx
+++ src/pages/NotificationsSettings.jsx
@@ -1,81 +1,94 @@
-
+import { useEffect, useState } from 'react'
 import CommonBackBtn from '../components/CommonBackBtn'
-import leftArrow from "../assets/images/left-arrow.png"
-
-import profileIcon from "../assets/images/profile.png"
-import bulbIcon from "../assets/images/bulb.png"
-import keyIcon from "../assets/images/key.png"
-import { NavLink } from 'react-router-dom'
 import CustomToggleSwitch from '../components/CustomToggleSwitch'
+import axiosInstance from '../utils/axios'
+import { useAuthStore } from '../store/auth'
+import { showToast } from '../utils/toast'
 
 function NotificationsSettings() {
+  const { user, login } = useAuthStore()
 
+  const [generalNotification, setGeneralNotification] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
-const menuItems = [
-  
-  {
-    label: "General Notification",
-    action: "navigate",
-    route: "/settings"
-  },
-  {
-    label: "Sound",
-    action: "navigate",
-    route: "/help"
-  },
-  {
-    label: "Sound Call",
-    action: "navigate",
-    route: "/privacy-policy"
-  },
-  {
-    label: "vibrate",
-    action: "navigate",
-    route: "/privacy-policy"
-  },
-];
+  useEffect(() => {
+    if (user?.generalNotification !== undefined) {
+      setGeneralNotification(user.generalNotification)
+    }
+  }, [user])
 
-const handleChange = (isChecked) => {
-    // alert(isChecked)
-}
+  const handleChange = async (isChecked) => {
+    setIsLoading(true)
+    setGeneralNotification(isChecked)
 
-return (
+    try {
+      const response = await axiosInstance.post("/api/v1/auth/profile/update-notification", {
+        generalNotification: isChecked,
+      })
 
-    <div className=' bg-sky-foam h-screen pb-16 relative'>
-        <div className='common-bg'></div>
+      if (response.data?.statusCode === 200 && response.data?.data) {
+        login({ user: response.data.data })
+        showToast.success(response.data.message)
+      }
+    } catch (err) {
+      showToast.error("Something went wrong")
+      setGeneralNotification(!isChecked)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
-        <div className='flex flex-col p-5  '>
-            <CommonBackBtn label='Settings'  />
+  const menuItems = [
+    {
+      label: "General Notification",
+      toggle: true
+    }
+  ];
+
+  return (
+    <div className='bg-sky-foam h-screen pb-16 relative'>
+      <div className='common-bg'></div>
+
+      <div className='flex flex-col p-5'>
+        <CommonBackBtn label='Settings' />
+      </div>
+
+      <div className='flex w-full justify-center items-center md:mt-24'>
+        <div className='bg-mint relative w-825 md:p-10 p-5 flex flex-col gap-7'>
+          <div className='common-right-design z-10 bottom-5 right-5'></div>
+
+          <div className='my-10'>
+            {
+              menuItems?.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    onClick={item.onclick && item.onclick}
+                    className='flex items-center justify-between max-w-md m-auto my-4 cursor-pointer gap-15'
+                  >
+                    <div className='flex gap-5 items-center'>
+                      <div className='sm:text-xl font-semibold'>{item.label}</div>
+                    </div>
+                    <div>
+                      {
+                        item.toggle ? (
+                          <CustomToggleSwitch
+                            checked={generalNotification}
+                            onChange={handleChange}
+                            disabled={isLoading}
+                          />
+                        ) : null
+                      }
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
-
-        <div className='flex  w-full justify-center items-center md:mt-24'>
-            <div className='bg-mint relative w-825 md:p-10 p-5 flex flex-col gap-7'>
-            <div className='common-right-design  z-10 bottom-5 right-5'></div>
-
-                                <div className='my-10'>
-                                {
-                                    menuItems?.map((item,index) => {
-                                        return  <div key={index} onClick={ item.onclick && item.onclick } className='flex items-center justify-between max-w-md m-auto my-4  cursor-pointer gap-15'>
-                                            <div className='flex gap-5  items-center'>
-                                                <div className='sm:text-xl font-semibold'>{item.label}</div>
-                                            </div>
-                                            
-                                            <div>
-                                                <CustomToggleSwitch onChange={handleChange}/>
-                                            </div>
-                                            
-                                        </div>
-                                       
-                                        
-                                    })
-                                }
-                            </div> 
-            </div>
-        </div>
+      </div>
     </div>
   )
 }
-
-
 
 export default NotificationsSettings
