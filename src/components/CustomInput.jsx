afddcbadad199cb398272113cb4df30b05dbc834
crea8ivedev <crea8ivedev@gmail.com>
2025-06-13 11:18:48 +0530

    debugging

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -172,7 +172,7 @@ function CustomInput({
         <ErrorMessage
           name={field.name}
           component='div'
-          className={cn("text-xs text-navy  font-semibold mt-1 ml- absolute -bottom-[20px]",errorStyle)}
+          className={cn("text-xs text-navy  font-semibold ",errorStyle)}
           aria-live="polite" aria-atomic="true"
         />
       </div>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({command,mode}) => {
+  const env = loadEnv(mode,process.cwd(), "")
+  return {
   plugins: [react(), eslint(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: env.VITE_API_BASE_URL,
+        changeOrigin: true,
+        secure: false,
+        ws:true
+      },
+    }
+  }
+}
 })
 
