9989739e2952bdd2ccf9c7badc300b8db68b3573
crea8ivedev <crea8ivedev@gmail.com>
2025-06-19 14:34:12 +0530

    Debugging

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "medicuro-app",
       script: "npm",
-      args: "run preview -- --port 3003",
+      args: "run preview -- --port 3003 --host 0.0.0.0",
       watch: false,
     },
   ],
