761a10e5c683f05ae7f3272041dfb941a890b66c
crea8ivedev <crea8ivedev@gmail.com>
2025-08-14 09:41:23 +0530

    Select box design issue fixed

--- src/pages/BookAppointment/views/CommonSelectBox.jsx
+++ src/pages/BookAppointment/views/CommonSelectBox.jsx
@@ -18,28 +18,31 @@ const CommonSelectBox = ({
         label && <div className="text-white font-bold mb-3">{label}</div>
       }
       
-      <select
-        name={name}
-        {...field}
-        {...props}
-        className={cn(
-          "bg-white w-full h-[45px] outline-0 appearance-none pr-10 ps-2 rounded-[3px] select-with-border border-teal-600",
-          className,
-        //   meta.touched && meta.error ? "border border-red-500" : ""
-        )}
-      >
-        {options.map((e) => (
-          <option key={e.value} value={e.value}>
-            {e.name}
-          </option>
-        ))}
-      </select>
+      <div className="relative">
+          <select
+            name={name}
+            {...field}
+            {...props}
+            className={cn(
+              "bg-white w-full h-[45px] outline-0 appearance-none pr-10 ps-2 rounded-[3px] select-with-border border-teal-600",
+              className,
+            //   meta.touched && meta.error ? "border border-red-500" : ""
+            )}
+          >
+            {options.map((e) => (
+              <option key={e.value} value={e.value}>
+                {e.name}
+              </option>
+            ))}
+          </select>
 
-      <img
-        src={down}
-        className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2"
-        alt="dropdown arrow"
-      />
+          <img
+            src={down}
+            className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2"
+            alt="dropdown arrow"
+          />
+
+      </div>
 {/* 
       <ErrorMessage
         name={name}
