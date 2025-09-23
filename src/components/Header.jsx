c6e3266d0b01a7b0a86a168211ae4aa3d1857393
GautamEncircle <crea8ivedev@gmail.com>
2025-09-23 21:23:18 +0530

    Removed sticky header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,50 +1,40 @@
 import { NavLink, useNavigate } from 'react-router-dom'
 import dummyProfile from '../assets/images/medicuro-logo.png'
-import { useAuthStore } from '../store/auth'
-import { useEffect, useState } from 'react'
-import axiosInstance from '../utils/axios'
-import { useAppointmentStore } from '../store/appointments'
+import { useAuthStore } from '../store/auth';
+import { useEffect, useState } from 'react';
+import axiosInstance from "../utils/axios"
+import { useAppointmentStore } from '../store/appointments';
 
-function Header({ setOpenMenu }) {
-  const { user } = useAuthStore()
-  const [nextAppointment, setNextAppointment] = useState(null)
+function Header({setOpenMenu}) {
+
+  const {user} = useAuthStore()
+  const [nextAppointment,setNextAppointment] = useState(null)
   const navigate = useNavigate()
-  const { upcomingAppointments } = useAppointmentStore()
+  const {upcomingAppointments} = useAppointmentStore()
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await axiosInstance.post(
-          '/api/v1/appointments/upcoming',
-        )
-        if (response.data?.statusCode == 200) {
-          const data = response.data?.firstUpcomingAppointment
-          setNextAppointment(data)
-        }
-      } catch (error) {}
-    }
-    fetchData()
-  }, [upcomingAppointments])
+        try {
+              const response = await axiosInstance.post("/api/v1/appointments/upcoming")
+              if(response.data?.statusCode == 200){
+                const data = response.data?.firstUpcomingAppointment
+                  setNextAppointment(data)
+              }
+            } catch (error) {
+        
+            }
+      }
+      fetchData()
+  },[upcomingAppointments])
 
   return (
-    <div className='sticky top-0 md:static md:h-93 md:ps-90 bg-white md:py-5 z-50 flex items-center'>
+    <div className='md:h-93 md:ps-90 bg-white md:py-5  flex items-center'>
       <div className='md:hidden w-full  flex justify-between items-center px-3 py-2'>
-        <div
-          className='text-5xl cursor-pointer'
-          onClick={(e) => {
-            e.stopPropagation()
-            setOpenMenu(true)
-          }}
-        >
-          &#8801;
-        </div>
-        <NavLink to={'/profile'}>
-          <img
-            src={user?.profilePic ?? dummyProfile}
-            alt='profile-image'
-            className='w-10 aspect-square rounded-circle'
-          />{' '}
-        </NavLink>
+        <div className='text-5xl cursor-pointer' onClick={(e) => {
+          e.stopPropagation();
+          setOpenMenu(true)
+        }}>&#8801;</div>
+        <NavLink to={"/profile"}><img src={user?.profilePic ??  dummyProfile} alt="profile-image" className='w-10 aspect-square rounded-circle' />  </NavLink>
       </div>
 
       <div className='md:flex gap-14 hidden'>
@@ -53,21 +43,14 @@ function Header({ setOpenMenu }) {
           <div className='font-semibold leading-4 '>{user?.fullName}</div>
         </div>
 
-        {nextAppointment && (
-          <div
-            onClick={() =>
-              navigate(`/view-appointment/Upcoming/${nextAppointment?.id}`)
-            }
-            className='md:flex flex-col bg-navy py-4 px-10 rounded-xl hidden cursor-pointer hover:opacity-90 hover:bg-white group hover:border-navy  border-navy  border-2 hover:border-2 hover:text-black'
-          >
-            <div className=' text-white font-light group-hover:text-black group-hover:font-semibold'>
-              Next appointment
-            </div>
+        {
+          nextAppointment &&  <div onClick={() => navigate(`/view-appointment/Upcoming/${nextAppointment?.id}`) } className='md:flex flex-col bg-navy py-4 px-10 rounded-xl hidden cursor-pointer hover:opacity-90 hover:bg-white group hover:border-navy  border-navy  border-2 hover:border-2 hover:text-black'>
+            <div className=' text-white font-light group-hover:text-black group-hover:font-semibold'>Next appointment</div>
             <div className='text-sky-cyan leading-4 font-light group-hover:text-navy group-hover:font-semibold'>
-              {nextAppointment?.date}
+              { nextAppointment?.date }
             </div>
           </div>
-        )}
+        }
       </div>
     </div>
   )
