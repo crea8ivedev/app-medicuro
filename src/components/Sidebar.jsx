ab2299c609212601f949bc7c769027f6feb7350f
GautamEncircle <crea8ivedev@gmail.com>
2025-09-23 12:02:35 +0530

    QA issues

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,50 +4,81 @@ import closeMenu from '../assets/images/close.svg'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { useNotificationStore } from '../store/notifications'
-import { useSocket } from '../context/socketContext';
+import { useSocket } from '../context/socketContext'
 import { useAuthStore } from '../store/auth'
 import { showToast } from '../utils/toast'
 
-import { Home as HomeIcon, User, PlusCircle, Bell, Settings, Info, LogOutIcon, XCircle } from "lucide-react";
-
+import {
+  Home as HomeIcon,
+  User,
+  PlusCircle,
+  Bell,
+  Settings,
+  Info,
+  LogOutIcon,
+  XCircle,
+} from 'lucide-react'
 
 function Sidebar({ openMenu, setOpenMenu }) {
-
   const navigate = useNavigate()
-  const { notifications: allNotifications, getNotifications } = useNotificationStore();
-  const [currentHoveredIcon, setCurrentHoveredIcon] = useState("")
+  const [isMobile, setIsMobile] = useState(false)
+  const { notifications: allNotifications, getNotifications } =
+    useNotificationStore()
+  const [currentHoveredIcon, setCurrentHoveredIcon] = useState('')
   const { user, logout } = useAuthStore()
 
-  const socket = useSocket();
+  const socket = useSocket()
 
-  const { pathname } = useLocation();
-  const [activeMenu, setActiveMenu] = useState("dashboard")
+  const { pathname } = useLocation()
+  const [activeMenu, setActiveMenu] = useState('dashboard')
 
   useEffect(() => {
     setActiveMenu(pathname)
   }, [pathname])
 
-  const checkIfActive = (url) => {
-    return activeMenu?.includes(url);
-  };
+  useEffect(() => {
+    const mobile = window.innerWidth < 768
+    setIsMobile(mobile)
+
+    const closeMenu = () => {
+      setOpenMenu(false)
+    }
+    document.addEventListener('click', closeMenu)
+    return () => {
+      document.removeEventListener('click', closeMenu)
+    }
+  }, [])
 
   useEffect(() => {
-    if (!socket) return;
-    if (user) {
-      socket.emit("register", user?.id)
+    if (isMobile) {
+      document.body.style.overflow = openMenu ? 'hidden' : ''
     }
-    socket.on("recieved_notification", getNotifications)
+    return () => {
+      document.body.style.overflow = ''
+    }
+  }, [isMobile, openMenu])
+
+  const checkIfActive = (url) => {
+    return activeMenu?.includes(url)
+  }
+
+  useEffect(() => {
+    if (!socket) return
+    if (user) {
+      socket.emit('register', user?.id)
+    }
+    socket.on('recieved_notification', getNotifications)
 
     const logoutWithToast = () => {
-      showToast.error("Password was changed by admin.");
-      logout();
+      showToast.error('Password was changed by admin.')
+      logout()
     }
 
-    socket.on("logout-user", logoutWithToast)
+    socket.on('logout-user', logoutWithToast)
 
     return () => {
-      socket && socket.off("recieved_notification", getNotifications)
-      socket && socket.off("logout-user", logoutWithToast)
+      socket && socket.off('recieved_notification', getNotifications)
+      socket && socket.off('logout-user', logoutWithToast)
     }
   }, [socket])
 
@@ -87,15 +118,15 @@ function Sidebar({ openMenu, setOpenMenu }) {
   //     link: "/notifications",
   //     newNotifications: allNotifications?.some(notification => !notification?.isRead),
   //     Icon : Bell,
-  //     isActive : checkIfActive("notifications") 
+  //     isActive : checkIfActive("notifications")
   //   },
   //   {
   //     label: 'Settings',
   //     isActive: checkIfActive("settings"),
   //     image: checkIfActive("settings") ? activesettings : settings,
   //     link: "/settings",
-  //     Icon : Settings , 
-  //     isActive : checkIfActive("settings") 
+  //     Icon : Settings ,
+  //     isActive : checkIfActive("settings")
   //   },
   //   {
   //     label: 'Help',
@@ -103,7 +134,7 @@ function Sidebar({ openMenu, setOpenMenu }) {
   //     image: checkIfActive("help") ? activeInformation : information,
   //     link: "/help",
   //     Icon : Info,
-  //     isActive : checkIfActive("help") 
+  //     isActive : checkIfActive("help")
   //   },
   //   {
   //     label: 'Logout',
@@ -114,45 +145,44 @@ function Sidebar({ openMenu, setOpenMenu }) {
   //   }
   // ];
 
-
   const [menuItems, setMenuItems] = useState([
     {
-      label: "Home",
-      link: "/dashboard",
+      label: 'Home',
+      link: '/dashboard',
       Icon: HomeIcon,
     },
     {
-      label: "Profile",
-      link: "/profile",
+      label: 'Profile',
+      link: '/profile',
       Icon: User,
     },
     {
-      label: "Book Now",
-      link: "/book-appointment",
+      label: 'Book Now',
+      link: '/book-appointment',
       Icon: PlusCircle,
     },
     {
-      label: "Notifications",
-      link: "/notifications",
+      label: 'Notifications',
+      link: '/notifications',
       Icon: Bell,
       newNotifications: allNotifications?.some((n) => !n?.isRead),
     },
     {
-      label: "Settings",
-      link: "/settings",
+      label: 'Settings',
+      link: '/settings',
       Icon: Settings,
     },
     {
-      label: "Help",
-      link: "/help",
+      label: 'Help',
+      link: '/help',
       Icon: Info,
     },
     {
-      label: "Logout",
-      link: "/log-out",
+      label: 'Logout',
+      link: '/log-out',
       Icon: LogOutIcon,
     },
-  ]);
+  ])
 
   const navigateToPage = (link) => {
     navigate(link)
@@ -160,15 +190,14 @@ function Sidebar({ openMenu, setOpenMenu }) {
   }
 
   useEffect(() => {
-
     // setOpenMenu(true)
 
     const closeMenu = () => {
       setOpenMenu(false)
     }
-    document.addEventListener("click", closeMenu)
+    document.addEventListener('click', closeMenu)
     return () => {
-      document.removeEventListener("click", closeMenu)
+      document.removeEventListener('click', closeMenu)
     }
   }, [])
 
@@ -189,13 +218,27 @@ function Sidebar({ openMenu, setOpenMenu }) {
         }}
         className='flex justify-between pe-2 whitespace-nowrap cursor-pointer'
       >
-        <div className={cn('md:flex ms-3 md:ms-0 mt-5 md:mt-0 items-center   md:w-66 text-center md:h-[93px] bg-navy',!openMenu && "hidden")} onClick={() => navigateToPage("/profile")}>
-          <img className='m-auto  object-cover rounded-circle h-[44px] w-[44px] object-cover hover:scale-110 transition-all duration-500  ' src={user?.profilePic ?? dummyProfile} alt='profile-pic' />
+        <div
+          className={cn(
+            'md:flex ms-3 md:ms-0 mt-5 md:mt-0 items-center   md:w-66 text-center md:h-[93px] bg-navy',
+            !openMenu && 'hidden',
+          )}
+          onClick={() => navigateToPage('/profile')}
+        >
+          <img
+            className='m-auto  object-cover rounded-circle h-[44px] w-[44px] object-cover hover:scale-110 transition-all duration-500  '
+            src={user?.profilePic ?? dummyProfile}
+            alt='profile-pic'
+          />
         </div>
       </div>
 
       <div className='flex overflow-hidden w-full'>
-        <div onMouseEnter={() => setOpenMenu(true)} className='h-screen bg-navy pt-3 hidden md:flex md:flex-col gap-8' onClick={(e) => e.stopPropagation()}>
+        <div
+          onMouseEnter={() => setOpenMenu(true)}
+          className='h-screen bg-navy pt-3 hidden md:flex md:flex-col gap-8'
+          onClick={(e) => e.stopPropagation()}
+        >
           {menuItems.map(({ Icon, ...item }, index) => {
             return (
               <div
@@ -203,20 +246,33 @@ function Sidebar({ openMenu, setOpenMenu }) {
                 key={index}
                 onClick={() => navigateToPage(item.link)}
               >
-                <div className='inline-block w-66 text-center relative'
+                <div
+                  className='inline-block w-66 text-center relative'
                   key={index}
                   onClick={() => navigateToPage(item.link)}
                 >
                   <div className='m-auto max-w-max relative'>
-                    <div className=' flex items-center cursor-pointer' onMouseEnter={() => handleMouseEnter(item.link)}
-                      onMouseLeave={() => handleMouseLeave()}>
-                      {
-                        Icon && <Icon className={cn("text-white w-[20px] h-[20px] m-auto hover:text-[#5ef1fa]", (checkIfActive(item.link) || currentHoveredIcon == item.link) && "text-[#5ef1fa]")} />
-                      }
+                    <div
+                      className=' flex items-center cursor-pointer'
+                      onMouseEnter={() => handleMouseEnter(item.link)}
+                      onMouseLeave={() => handleMouseLeave()}
+                    >
+                      {Icon && (
+                        <Icon
+                          className={cn(
+                            'text-white w-[20px] h-[20px] m-auto hover:text-[#5ef1fa]',
+                            (checkIfActive(item.link) ||
+                              currentHoveredIcon == item.link) &&
+                              'text-[#5ef1fa]',
+                          )}
+                        />
+                      )}
                     </div>
-                    {
-                      item?.newNotifications && <div className='absolute top-1 right-0 h-05 w-05 bg-red-500 rounded-circle'> </div>
-                    }
+                    {item?.newNotifications && (
+                      <div className='absolute top-1 right-0 h-05 w-05 bg-red-500 rounded-circle'>
+                        {' '}
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
@@ -231,13 +287,16 @@ function Sidebar({ openMenu, setOpenMenu }) {
           )}
           onClick={(e) => e.stopPropagation()}
         >
-          <div className='flex justify-end cursor-pointer' onClick={() => setOpenMenu(false)}>
+          <div
+            className='flex justify-end cursor-pointer'
+            onClick={() => setOpenMenu(false)}
+          >
             {/* <img
               src={closeMenu}
               className='ms-auto pe-3 pt-3'
               alt=''
             /> */}
-            <XCircle size={27} className='ms-auto pe-3 pt-3 text-white'/>
+            <XCircle size={27} className='ms-auto pe-3 pt-3 text-white' />
           </div>
           <div className='md:mt-[75px] mt-[60px] pe-40 flex flex-col md:gap-8 gap-8  border-t-[0.5px] border-aqua pt-8 md:border-0 md:pt-0'>
             {menuItems.map(({ Icon, ...item }, index) => {
@@ -250,14 +309,32 @@ function Sidebar({ openMenu, setOpenMenu }) {
                   onMouseLeave={() => handleMouseLeave()}
                 >
                   <div className='relative sm:hidden'>
-                    {
-                      Icon && <Icon className={cn("text-white w-[20px] h-[20px]  ", (checkIfActive(item.link) || currentHoveredIcon == item.link) && "text-[#5ef1fa]")} />
-                    }
-                    {
-                      item?.newNotifications && <div className='absolute top-1 right-0 h-05 w-05 bg-red-500 rounded-circle'> </div>
-                    }
+                    {Icon && (
+                      <Icon
+                        className={cn(
+                          'text-white w-[20px] h-[20px]  ',
+                          (checkIfActive(item.link) ||
+                            currentHoveredIcon == item.link) &&
+                            'text-[#5ef1fa]',
+                        )}
+                      />
+                    )}
+                    {item?.newNotifications && (
+                      <div className='absolute top-1 right-0 h-05 w-05 bg-red-500 rounded-circle'>
+                        {' '}
+                      </div>
+                    )}
                   </div>
-                  <div className={cn('md:inline-block text-white hover:text-aqua flex items-center max-h-[19px]', (checkIfActive(item.link) || currentHoveredIcon == item.link) && "text-aqua")}>{item.label}</div>
+                  <div
+                    className={cn(
+                      'md:inline-block text-white hover:text-aqua flex items-center max-h-[19px]',
+                      (checkIfActive(item.link) ||
+                        currentHoveredIcon == item.link) &&
+                        'text-aqua',
+                    )}
+                  >
+                    {item.label}
+                  </div>
                 </div>
               )
             })}
