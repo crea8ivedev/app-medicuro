e925c068533ff42cf4140c1c9485f9f6679f3fc5
GautamEncircle <crea8ivedev@gmail.com>
2025-09-25 12:13:22 +0530

    Debugging

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,51 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import eslint from "vite-plugin-eslint";
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import eslint from 'vite-plugin-eslint'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), '')
   return {
-  plugins: [react(), eslint(), tailwindcss()],
- server: {
-  host: "0.0.0.0",
-  proxy: {
-    '/api': {
-      target: env.VITE_API_BASE_URL,
-      changeOrigin: true,
-      secure: env.VITE_NODE_ENV === "production",
-      ws: true,
+    plugins: [react(), eslint(), tailwindcss()],
+    server: {
+      host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: env.VITE_NODE_ENV === 'production',
+          ws: true,
+        },
+        '/google': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: env.VITE_NODE_ENV === 'production',
+        },
+        '/facebook': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: env.VITE_NODE_ENV === 'production',
+        },
+        '/auth': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: env.VITE_NODE_ENV === 'production',
+        },
+      },
+      https: {
+        key: fs.readFileSync('./cert.key'),
+        cert: fs.readFileSync('./cert.crt'),
+      },
+      allowedHosts: ['ee5ee308d518.ngrok-free.app'],
     },
-    '/google': {
-      target: env.VITE_API_BASE_URL,
-      changeOrigin: true,
-      secure: env.VITE_NODE_ENV === "production",
+    preview: {
+      allowedHosts: [
+        'medicuro-app.encircledev.com',
+        'http://192.168.1.125:5173',
+        '5b0ff6f69b36.ngrok-free.app',
+      ],
     },
-    '/facebook': {
-      target: env.VITE_API_BASE_URL,
-      changeOrigin: true,
-      secure: env.VITE_NODE_ENV === "production",
-    },
-    '/auth': {
-      target: env.VITE_API_BASE_URL,
-      changeOrigin: true,
-      secure: env.VITE_NODE_ENV === "production",
-    }
-  },
-  allowedHosts : ["ee5ee308d518.ngrok-free.app"]
-},
-  preview : {
-    allowedHosts : ["medicuro-app.encircledev.com","http://192.168.1.125:5173","5b0ff6f69b36.ngrok-free.app"]
   }
-}
 })
-
